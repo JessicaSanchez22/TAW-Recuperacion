146c100178e87656ea32cb61c0adf170659a20e5
JessicaSanchez22 <32375442+JessicaSanchez22@users.noreply.github.com>
2018-08-21 23:48:14 -0500

    Base de datos

--- inventarioj.sql
+++ inventarioj.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
--- https://www.phpmyadmin.net/
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 18-06-2018 a las 15:38:29
--- Versión del servidor: 10.1.31-MariaDB
--- Versión de PHP: 7.2.3
+-- Servidor: localhost
+-- Tiempo de generación: 21-08-2018 a las 23:47:53
+-- Versión del servidor: 5.7.23-0ubuntu0.16.04.1
+-- Versión de PHP: 7.0.30-0ubuntu0.16.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -29,24 +27,17 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `categoria` (
-  `id_categoria` int(11) NOT NULL,
-  `nombre_categoria` varchar(255) NOT NULL,
-  `descripcion_categoria` varchar(255) NOT NULL,
-  `date_added` datetime NOT NULL,
-  `id_tienda` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `categoria`
 --
 
-INSERT INTO `categoria` (`id_categoria`, `nombre_categoria`, `descripcion_categoria`, `date_added`, `id_tienda`) VALUES
-(1, 'Linea blanca', 'Limpieza', '2018-05-08 00:00:00', 1),
-(2, 'Linea blanca', 'Linea blanca y colchoneria', '2018-05-22 00:00:00', 2),
-(3, 'Electrodomesticos', 'Electrodomesticos', '2018-05-15 00:00:00', 1),
-(5, 'Cosmeticos', 'Cosmeteria', '0000-00-00 00:00:00', 1),
-(6, 'Belleza', 'Belleza', '0000-00-00 00:00:00', 2),
-(7, 'Ropa', 'Ropa variedad', '0000-00-00 00:00:00', 2);
+INSERT INTO `categoria` (`id`, `nombre`) VALUES
+(1, 'belleza'),
+(2, 'electrodomesticos');
 
 -- --------------------------------------------------------
 
@@ -60,39 +51,150 @@ CREATE TABLE `clientes` (
   `apellido` varchar(100) NOT NULL,
   `email` varchar(50) NOT NULL,
   `direccion` varchar(100) NOT NULL,
-  `telefono` varchar(10) NOT NULL,
-  `date_added` datetime NOT NULL,
-  `id_tienda` int(11) NOT NULL
+  `telefono` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `clientes`
 --
 
-INSERT INTO `clientes` (`id`, `nombre`, `apellido`, `email`, `direccion`, `telefono`, `date_added`, `id_tienda`) VALUES
-(1, 'lkjn', 'ljkblkj', 'bjkb@uhuh', 'bj', 'bkjhb', '2018-06-18 09:07:25', 0),
-(2, 'jkbj', 'bkhjbkj', 'hhjbj@kjskj', 'hbjh', 'bkhjbkj', '2018-06-18 09:07:41', 0),
-(4, 'jess', 'iuhi', 'uho@hu', 'uiho', 'ug', '2018-06-18 09:10:54', 0),
-(5, 'ii', 'hiuh', 'uho@hu', 'iuh', 'uh', '2018-06-18 09:12:27', 0),
-(6, 'Marco', 'Perez', 'marco@hotmail.com', 'Calle politecnico #210', '834567899', '2018-06-18 09:16:45', 1),
-(7, 'Angel', 'Sanchez', '1530466@upv.edu.mx', 'Calle politecnico #110', '8645515143', '2018-06-18 09:40:09', 2);
+INSERT INTO `clientes` (`id`, `nombre`, `apellido`, `email`, `direccion`, `telefono`) VALUES
+(6, 'Marco', 'Perez Perez', 'marco@hotmail.com', 'Calle politecnico #210', '834567891'),
+(7, 'Angel', 'Sanchez', '1530466@upv.edu.mx', 'Calle politecnico #110', '8645515143'),
+(9, 'Jose Marco', 'Fuentes Escamilla', 'jo@gmail.com', 'Calle politecnico #110', '3874698'),
+(11, 'Juan Miguel', 'Israel Mendez', '15300@gmail.com', 'Direccion ', '8346253'),
+(12, 'Juanita', 'Garza Zapata', '1530466@upv.edu.mx', 'Calle jose de escandon', '82452416');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `historial`
+-- Estructura de tabla para la tabla `compra`
 --
 
-CREATE TABLE `historial` (
-  `id_historial` int(11) NOT NULL,
+CREATE TABLE `compra` (
+  `id` int(11) NOT NULL,
   `id_producto` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `fecha` datetime NOT NULL,
-  `nota` varchar(255) NOT NULL,
-  `referencia` varchar(255) NOT NULL,
+  `nombre_producto` varchar(255) NOT NULL,
+  `codigo_producto` int(11) NOT NULL,
   `cantidad` int(11) NOT NULL,
-  `id_tienda` int(11) NOT NULL
+  `precio_compra` int(11) NOT NULL,
+  `precio_venta` int(11) NOT NULL,
+  `margen` int(11) NOT NULL,
+  `id_proveedor` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `compra`
+--
+
+INSERT INTO `compra` (`id`, `id_producto`, `nombre_producto`, `codigo_producto`, `cantidad`, `precio_compra`, `precio_venta`, `margen`, `id_proveedor`) VALUES
+(1, 1, 'Sombra para ojos', 580, 3, 100, 200, 100, 1),
+(1, 1, 'Sombra para ojos', 580, 5, 100, 200, 100, 1),
+(1, 5, 'Mascarilla biore', 1453, 2, 316, 400, 84, 1),
+(1, 6, 'rubor', 1298, 6, 1080, 1200, 120, 2),
+(1, 12, 'Delineador', 1, 5, 125, 250, 125, 2),
+(3, 2, 'Sombras para ojos', 200, 1, 710, 520, 0, 1),
+(4, 2, 'Sombras para ojos', 200, 10, 7100, 8000, 900, 1),
+(6, 5, 'Mascarilla biore', 1453, 8, 1264, 1600, 336, 2),
+(4, 6, 'rubor', 1298, 1, 180, 120, 0, 2),
+(6, 2, 'Sombras para ojos', 200, 1, 710, 500, 0, 1),
+(7, 4, 'pestaÃ±as postizas', 67854, 6, 720, 3600, 2880, 2),
+(8, 12, 'Delineador', 1, 1, 25, 10, 0, 1),
+(9, 2, 'Sombras para ojos', 200, 2, 1420, 400, 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `compras`
+--
+
+CREATE TABLE `compras` (
+  `id` int(11) NOT NULL,
+  `id_proveedor` int(11) DEFAULT NULL,
+  `total_compra` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `compras`
+--
+
+INSERT INTO `compras` (`id`, `id_proveedor`, `total_compra`) VALUES
+(1, 1, 1200),
+(3, 1, 1721),
+(4, 1, 710),
+(6, 1, 7280),
+(7, 1, 1974),
+(8, 1, 720),
+(9, 1, 25),
+(10, 1, 1420);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedido`
+--
+
+CREATE TABLE `pedido` (
+  `id` int(11) NOT NULL,
+  `nombre_producto` varchar(50) NOT NULL,
+  `codigo_producto` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio_compra` int(11) NOT NULL,
+  `precio_venta` int(11) NOT NULL,
+  `margen` int(11) NOT NULL,
+  `id_proveedor` int(11) NOT NULL,
+  `fecha_pedido` date NOT NULL,
+  `fecha_entrega` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `pedido`
+--
+
+INSERT INTO `pedido` (`id`, `nombre_producto`, `codigo_producto`, `cantidad`, `precio_compra`, `precio_venta`, `margen`, `id_proveedor`, `fecha_pedido`, `fecha_entrega`) VALUES
+(1, 'Sombras para ojos', 200, 3, 200, 300, 100, 1, '2018-08-21', '2018-08-28'),
+(1, 'rubor', 1298, 3, 540, 360, 0, 1, '2018-08-21', '2018-08-28'),
+(1, 'rubor', 1298, 3, 540, 360, 0, 1, '2018-08-21', '2018-08-28'),
+(1, 'rubor', 1298, 3, 540, 360, 0, 1, '2018-08-21', '2018-08-28'),
+(1, 'rubor', 1298, 3, 540, 360, 0, 1, '2018-08-21', '2018-08-28'),
+(1, 'rubor', 1298, 3, 540, 360, 0, 1, '2018-08-21', '2018-08-28'),
+(1, 'rubor', 1298, 3, 540, 360, 0, 1, '2018-08-21', '2018-08-28'),
+(1, 'Delineador', 1, 50, 1250, 1000, 0, 1, '2018-08-21', '2018-08-28'),
+(0, 'Mascarilla biore', 1453, 7, 1106, 63, 0, 1, '2018-08-07', '2018-08-08'),
+(0, 'Sombras para ojos', 200, 2, 1420, 4, 0, 1, '2018-08-16', '2018-08-17'),
+(0, 'Sombras para ojos', 200, 8, 5680, 4000, 0, 2, '2018-08-24', '2018-08-14'),
+(1, 'Sombras para ojos', 200, 4, 2840, 2000, 0, 2, '2018-08-21', '2018-08-22'),
+(2, 'Sombras para ojos', 200, 1, 710, 1, 0, 1, '2018-08-18', '2018-08-18'),
+(2, 'Sombras para ojos', 200, 1, 710, 1, 0, 1, '2018-08-10', '2018-08-03'),
+(4, 'Brochas para sombras', 102, 3, 1050, 6, 0, 2, '2018-08-10', '2018-08-10'),
+(4, 'Mascarilla', 555, 1, 120, 10, 0, 1, '2018-08-10', '2018-08-10'),
+(5, 'banditas', 3535, 3, 45, 90, 45, 2, '2018-08-23', '2018-08-14'),
+(6, 'Delineador', 1, 25, 625, 3000, 2375, 2, '2018-08-21', '2018-08-23');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `id` int(11) NOT NULL,
+  `fecha_pedido` date NOT NULL,
+  `fecha_entrega` date NOT NULL,
+  `total_pedido` int(11) NOT NULL,
+  `id_proveedor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `pedidos`
+--
+
+INSERT INTO `pedidos` (`id`, `fecha_pedido`, `fecha_entrega`, `total_pedido`, `id_proveedor`) VALUES
+(1, '2018-08-21', '2018-08-28', 2500, 1),
+(3, '2018-08-18', '2018-08-18', 1420, 1),
+(4, '2018-08-10', '2018-08-10', 1170, 2),
+(5, '2018-08-23', '2018-08-14', 45, 2),
+(6, '2018-08-21', '2018-08-23', 625, 2);
 
 -- --------------------------------------------------------
 
@@ -104,83 +206,45 @@ CREATE TABLE `productos` (
   `id` int(11) NOT NULL,
   `codigo_producto` int(11) NOT NULL,
   `nombre` varchar(100) NOT NULL,
-  `date_added` datetime NOT NULL,
   `precio_producto` int(11) NOT NULL,
   `cantidad_stock` int(11) NOT NULL,
-  `id_categoria` int(11) NOT NULL,
-  `id_tienda` int(11) NOT NULL
+  `id_categoria` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`id`, `codigo_producto`, `nombre`, `date_added`, `precio_producto`, `cantidad_stock`, `id_categoria`, `id_tienda`) VALUES
-(1, 580, 'Labiales mate', '2018-06-06 07:26:25', 300, 11, 1, 1),
-(2, 200, 'Sombras para ojos', '2018-06-18 04:50:52', 710, 1, 6, 2),
-(4, 67854, 'pestaÃ±as postizas', '2018-06-06 09:08:18', 120, 15, 1, 1),
-(5, 1453, 'Mascarilla biore', '2018-06-16 03:23:45', 158, 9, 1, 1),
-(6, 1298, 'rubor', '2018-06-14 12:18:37', 180, 77, 5, 1),
-(7, 102, 'Brochas para sombras', '2018-06-14 10:06:52', 350, 23, 5, 1),
-(8, 2701, 'Licuadora', '2018-06-16 07:15:26', 1500, 13, 3, 1),
-(9, 111, 'Refrigerador', '2018-06-17 07:30:06', 17000, 4, 3, 1),
-(10, 555, 'Mascarilla', '2018-06-16 07:20:52', 120, 13, 5, 5),
-(11, 1203, 'Mascarilla de miel', '2018-06-18 04:50:31', 50, 18, 6, 2);
+INSERT INTO `productos` (`id`, `codigo_producto`, `nombre`, `precio_producto`, `cantidad_stock`, `id_categoria`) VALUES
+(2, 200, 'Sombras para ojos', 710, 14, 1),
+(4, 67854, 'pestaÃ±as postizas', 120, 13, 1),
+(5, 1453, 'Mascarilla biore', 158, 17, 1),
+(6, 1298, 'rubor', 180, 48, 1),
+(7, 102, 'Brochas para sombras', 350, 22, 1),
+(9, 111, 'Refrigerador', 17000, 2, 2),
+(10, 555, 'Mascarilla', 120, 13, 1),
+(11, 1203, 'Mascarilla de miel', 50, 18, 1),
+(12, 1, 'Delineador', 25, 21, 1),
+(13, 3535, 'banditas', 15, 20, 1);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tienda`
+-- Estructura de tabla para la tabla `proveedores`
 --
 
-CREATE TABLE `tienda` (
+CREATE TABLE `proveedores` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `date_added` datetime NOT NULL,
-  `estado` varchar(20) NOT NULL
+  `nombre` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `tienda`
+-- Volcado de datos para la tabla `proveedores`
 --
 
-INSERT INTO `tienda` (`id`, `nombre`, `date_added`, `estado`) VALUES
-(1, 'tiendaA1', '2018-06-11 07:19:48', 'activa'),
-(2, 'tienda2', '2018-06-27 09:31:12', 'activa'),
-(4, 'miTienda', '2018-06-18 04:48:46', 'activa'),
-(5, 'GranD', '2018-06-18 04:48:58', 'desactiva'),
-(10, 'Walmart', '2018-06-18 04:42:34', 'desactiva'),
-(11, 'JessaBeauty', '2018-06-18 04:32:09', 'activa'),
-(12, 'Sally', '2018-06-18 04:41:11', 'desactiva');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `first_name` varchar(20) NOT NULL,
-  `last_name` varchar(20) NOT NULL,
-  `usuario` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `user_email` varchar(64) NOT NULL,
-  `date_added` datetime NOT NULL,
-  `id_tienda` int(11) NOT NULL,
-  `superadmin` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `users`
---
-
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `usuario`, `password`, `user_email`, `date_added`, `id_tienda`, `superadmin`) VALUES
-(2, 'Jessica', 'Sanchez Garcia', 'admin', 'admin', 'admin@gmail.com', '2018-06-27 09:31:12', 1, 1),
-(3, 'Mario', 'Rodriguez', 'mario', 'mario', 'mario@gmail.com', '2018-06-28 05:17:13', 1, 0),
-(4, 'Pedro', 'Perez', 'pedro', '0', 'pedro@gmail.com', '0000-00-00 00:00:00', 1, 0),
-(6, 'JosÃ© Marco', 'Fuentes Escamilla', 'marco', 'marco', 'marco@gmail.com', '2018-06-16 07:34:53', 1, 1),
-(7, 'Jessa', 'Saga', 'jessa', 'jessa', 'jessa@gmail.com', '2018-06-18 05:00:24', 2, 0);
+INSERT INTO `proveedores` (`id`, `nombre`) VALUES
+(1, 'Sally Beauty Supply'),
+(2, 'Renatta Cosmeticos');
 
 -- --------------------------------------------------------
 
@@ -291,7 +355,18 @@ INSERT INTO `venta` (`id_venta`, `id_producto`, `codigo_producto`, `nombre_produ
 (43, 6, 1298, 'rubor', 5, 900),
 (43, 6, 1298, 'rubor', 5, 900),
 (43, 1, 580, 'Labiales mate', 3, 900),
-(43, 1, 580, 'Labiales mate', 2, 600);
+(43, 1, 580, 'Labiales mate', 2, 600),
+(44, 4, 67854, 'pestaÃ±as postizas', 8, 960),
+(44, 6, 1298, 'rubor', 6, 1080),
+(44, 6, 1298, 'rubor', 6, 1080),
+(44, 6, 1298, 'rubor', 6, 1080),
+(44, 6, 1298, 'rubor', 6, 1080),
+(44, 6, 1298, 'rubor', 6, 1080),
+(44, 6, 1298, 'rubor', 6, 1080),
+(45, 2, 200, 'Sombras para ojos', 6, 4260),
+(47, 9, 111, 'Refrigerador', 2, 34000),
+(48, 7, 102, 'Brochas para sombras', 1, 350),
+(49, 5, 1453, 'Mascarilla biore', 2, 316);
 
 -- --------------------------------------------------------
 
@@ -301,9 +376,7 @@ INSERT INTO `venta` (`id_venta`, `id_producto`, `codigo_producto`, `nombre_produ
 
 CREATE TABLE `ventas` (
   `id` int(11) NOT NULL,
-  `date_added` datetime NOT NULL,
   `total_venta` int(11) NOT NULL,
-  `id_tienda` int(11) NOT NULL,
   `id_cliente` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -311,20 +384,17 @@ CREATE TABLE `ventas` (
 -- Volcado de datos para la tabla `ventas`
 --
 
-INSERT INTO `ventas` (`id`, `date_added`, `total_venta`, `id_tienda`, `id_cliente`) VALUES
-(1, '2018-06-13 07:17:16', 350, 1, 0),
-(33, '0000-00-00 00:00:00', 2580, 1, 0),
-(34, '2018-06-18 04:13:12', 1420, 2, 0),
-(35, '2018-06-18 04:20:12', 2130, 2, 0),
-(36, '2018-06-18 04:24:01', 7100, 2, 0),
-(37, '2018-06-18 03:09:56', 3000, 1, 6),
-(38, '2018-06-18 03:13:41', 12710, 1, 6),
-(39, '2018-06-18 03:16:22', 2738, 1, 6),
-(40, '2018-06-18 03:17:43', 300, 1, 6),
-(41, '2018-06-18 03:18:12', 1200, 1, 6),
-(42, '2018-06-18 03:19:40', 1736, 1, 6),
-(43, '2018-06-18 03:32:03', 7200, 1, 6),
-(44, '2018-06-18 03:36:51', 9600, 1, 6);
+INSERT INTO `ventas` (`id`, `total_venta`, `id_cliente`) VALUES
+(41, 1200, 6),
+(42, 1736, 6),
+(43, 7200, 6),
+(44, 9600, 6),
+(45, 9840, 6),
+(46, 6960, 6),
+(47, 1800, 6),
+(48, 36400, 6),
+(49, 2750, 6),
+(50, 2116, 6);
 
 --
 -- Índices para tablas volcadas
@@ -334,7 +404,7 @@ INSERT INTO `ventas` (`id`, `date_added`, `total_venta`, `id_tienda`, `id_client
 -- Indices de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  ADD PRIMARY KEY (`id_categoria`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `clientes`
@@ -343,10 +413,16 @@ ALTER TABLE `clientes`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `historial`
+-- Indices de la tabla `compras`
 --
-ALTER TABLE `historial`
-  ADD PRIMARY KEY (`id_historial`);
+ALTER TABLE `compras`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `productos`
@@ -355,15 +431,9 @@ ALTER TABLE `productos`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `tienda`
+-- Indices de la tabla `proveedores`
 --
-ALTER TABLE `tienda`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `users`
---
-ALTER TABLE `users`
+ALTER TABLE `proveedores`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -380,45 +450,37 @@ ALTER TABLE `ventas`
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
--- AUTO_INCREMENT de la tabla `historial`
+-- AUTO_INCREMENT de la tabla `compras`
 --
-ALTER TABLE `historial`
-  MODIFY `id_historial` int(11) NOT NULL AUTO_INCREMENT;
-
+ALTER TABLE `compras`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
--- AUTO_INCREMENT de la tabla `tienda`
+-- AUTO_INCREMENT de la tabla `proveedores`
 --
-ALTER TABLE `tienda`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
--- AUTO_INCREMENT de la tabla `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
+ALTER TABLE `proveedores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT de la tabla `ventas`
 --
 ALTER TABLE `ventas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
-COMMIT;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
